f7deebeaeab5cc7bf67c92a368e1a4a370d37299
momchilasenov <momchil276@gmail.com>
2021-04-23 20:52:21 +0300

    Added exercise assignments

---
+++ 01.CSharp-Advanced/04.Streams,Files-and-Directories/02.Streams,Files-and-Directories-Exercise/02.Line-Numbers/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _02.Line_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("../../../input.txt");
+            string[] newLine = new string[lines.Length];
+
+            int lettersCount = 0;
+            int marksCount = 0;
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string line = lines[i];
+
+                lettersCount = GetLetters(line);
+                marksCount = GetMarks(line);
+
+                newLine[i] = $"Line {i+1}: {lines[i]} ({lettersCount})({marksCount})";
+            }
+
+            File.WriteAllLines("../../../output.txt", newLine);
+
+        }
+
+        static int GetLetters(string line)
+        {
+            int counter = 0;
+
+            for (int j = 0; j < line.Length; j++)
+            {
+                if (char.IsLetter(line[j]))
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+
+        static int GetMarks(string line)
+        {
+            int counter = 0;
+            char[] marks = new char[] { ',', '.', '-', '?', '!', ':', '"', '\'' };
+
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (marks.Contains(line[i]))
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+    }
+}
---
+++ 01.CSharp-Advanced/04.Streams,Files-and-Directories/02.Streams,Files-and-Directories-Exercise/03.WordCount/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Collections.Generic;
+
+namespace _03.WordCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] wordsToFind = File.ReadAllLines("../../../words.txt");
+            string[] lines = File.ReadAllLines("../../../text.txt");
+
+            Dictionary<string, int> wordCount = new Dictionary<string, int>();
+
+            for (int i = 0; i < wordsToFind.Length; i++)
+            {
+                string wordToFind = wordsToFind[i].ToLower().Trim();
+
+                if (!wordCount.ContainsKey(wordToFind))
+                {
+                    wordCount.Add(wordToFind, 0);
+                }
+                
+            }
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string line = lines[i].ToLower();
+
+                string[] wordsArray = SplitWords(line);
+
+                wordCount = GetWordsCount(wordCount, wordsToFind, wordsArray);
+            }
+
+            GetActualResult(wordCount, wordsToFind);
+
+            GetExpectedResult(wordCount, wordsToFind);
+
+        }
+
+        static Dictionary<string, int> GetWordsCount(Dictionary<string, int> wordCount, string[] wordsToFind, string[] wordsArray)
+        {
+            for (int i = 0; i < wordsArray.Length; i++)
+            {
+                string currentWord = wordsArray[i].Trim();
+
+                for (int j = 0; j < wordsToFind.Length; j++)
+                {
+                    string wordToFind = wordsToFind[j].ToLower().Trim();
+
+                    if (currentWord == wordToFind)
+                    {
+                        if (wordCount.ContainsKey(wordToFind)) 
+                        {
+                            wordCount[wordToFind]++;
+                        }
+                    }
+                }
+            }
+
+            return wordCount;
+        }
+
+        static string[] SplitWords(string line)
+        {
+            string[] wordsArray = line
+                .Split(new char[] { ',', ':', ' ', '!', '?', '.', '-' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            return wordsArray;
+        }
+
+        static void GetActualResult(Dictionary<string, int> wordCount, string[] wordsToFind)
+        {
+            int indexCount = 0;
+            string[] actualResult = new string[wordsToFind.Length];
+
+            foreach (var word in wordCount) //itterate through dictionary to extract key and value
+            {
+                actualResult[indexCount] = $"{word.Key} - {word.Value}";
+                indexCount++;
+            }
+
+            File.WriteAllLines("../../../actualResults.txt", actualResult);
+        }
+
+        static void GetExpectedResult(Dictionary<string, int> wordCount, string[] wordsToFind)
+        {
+            int indexCount = 0;
+            string[] expectedResult = new string[wordsToFind.Length];
+
+            wordCount = wordCount.OrderByDescending(x => x.Value).ToDictionary(k => k.Key, v => v.Value);
+
+            foreach (var word in wordCount)
+            {
+                expectedResult[indexCount] = $"{word.Key} - {word.Value}";
+                indexCount++;
+            }
+
+            File.WriteAllLines("../../../expectedResults.txt", expectedResult);
+        }
+    }
+}
